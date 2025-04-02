2daac72a55512933d343f7ca56cf9113291240bf
Gabriel-CB08 <gabriel.cb0802@gmail.com>
2025-04-02 19:57:02 -0300

    PPTR-25 <message>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home'
 import Login from './pages/Login'
@@ -11,7 +10,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,31 @@
 import { Link } from 'react-router-dom';
+import LogoTR from './LogoTR.png';
+
+function Navbar() {
+  return (
+    <nav className="bg-gray-900 text-white p-4 shadow-md mt-6 mb-4"> 
+      <div className="container mx-auto flex justify-between items-center">    
+        <h1 className="text-xl font-bold">Team Rocket Trade</h1>
+        <div className="space-x-4">
+          <Link to="/" className="hover:text-gray-300">Inicio</Link>  |
+          |  <Link to="/pokemons" className="hover:text-gray-300">Pokemons</Link>  |
+          |  <Link to="/colecciones" className="hover:text-gray-300">Colecciones</Link>  |
+          |  <Link to="/tienda" className="hover:text-gray-300">Tienda</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
 
 function Home() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Titulo de prueba</h1>
-        <nav>
-          <Link to="/login">Login</Link> | <Link to="/register">Registrarse</Link>
+      <Navbar />
+      <header className="text-center">
+        <h1>Ingresa a tu cuenta o Únete al Team Rocket</h1>
+        <nav className="mt-4">
+          <Link to="/login" className="mx-2 text-blue-500">Login</Link>
+          <Link to="/register" className="mx-2 text-blue-500">Registrarse</Link>
         </nav>
       </header>
     </div>

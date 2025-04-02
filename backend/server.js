3ca05e13a14530419684d3f857d9cfd8f2fa87b7
martin-peralta <martperalta@alumnos.uai.cl>
2025-04-02 19:10:30 -0300

    PPTR-41 Configurar Express para manejar autentificación

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './TRdb.js';
+import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
 connectDB(); 
@@ -9,6 +10,10 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+
 
 app.get('/', (req, res) => {
     res.send('API funcionando correctamente');

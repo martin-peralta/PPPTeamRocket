6be8bbfd530a4f96c912fa5beae1c29284cfbef8
Gabriel-CB08 <gabriel.cb0802@gmail.com>
2025-04-08 21:52:45 -0400

    PPTR-44 <message>

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import './App.css';
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
+import Navbar from './Components/Navbar';
+import Home from './Screens/Home';
+
 
 function App() {
-  const [users, setUsers] = useState([]);
-  return (
-    <div className="App">
-      <header className="App-header">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login users={users} />} />
-          <Route path="/register" element={<Register setUsers={setUsers} />} />
-        </Routes>
-      </Router>
-
-          
-    
-      </header>
+  return <div className="App">
+    <Navbar />
+    <Home />
     </div>
-  );
 }
 
 export default App;
---
+++ src/Screens/Home.js
@@ -0,0 +1,77 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Link } from 'react-router-dom';
+// import LogoTR from './LogoTR.png';
+
+// function Navbar() {
+//   return (
+//     <nav className="bg-gray-900 text-white p-4 shadow-md mt-6 mb-4"> 
+//       <div className="container mx-auto flex justify-between items-center">    
+//         <h1 className="text-xl font-bold">Team Rocket Trade</h1>
+//         <div className="space-x-4">
+//           <Link to="/" className="hover:text-gray-300">Inicio</Link>  |
+//           |  <Link to="/pokemons" className="hover:text-gray-300">Pokemons</Link>  |
+//           |  <Link to="/colecciones" className="hover:text-gray-300">Colection</Link>  |
+//           |  <Link to="/tienda" className="hover:text-gray-300">Shop</Link>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// function Home() {
+//   return (
+//     <div className="App">
+//       <Navbar />
+//       <header className="text-center">
+//         <h1>Log in or Sign up to Team Rocket</h1>
+//         <nav className="mt-4">
+//           <Link to="/login" className="mx-2 text-blue-500">Login</Link>
+//           <Link to="/register" className="mx-2 text-blue-500">Register</Link>
+//         </nav>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default Home;

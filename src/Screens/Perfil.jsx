fca885a18e70e468e62139f9f0dcc57fc292d7a7
Gabriel-CB08 <gabriel.cb0802@gmail.com>
2025-04-16 17:49:15 -0400

    PPTR-61 footer y Navbar por arreglar

--- src/Screens/Perfil.jsx
+++ src/Screens/Perfil.jsx
@@ -1,7 +1,6 @@
 const Perfil = () => {
   return (
     <div className="flex flex-col min-h-screen">
-
       <main className="flex-grow bg-white text-center py-12">
         <h1 className="text-4xl font-bold mb-4">Mi Perfil</h1>
         <p className="text-lg text-gray-700 mb-8">
@@ -14,8 +13,6 @@ const Perfil = () => {
           <p><span className="font-bold">Equipo Pokémon:</span> Pikachu, Charizard, Bulbasaur, Squirtle</p>
         </div>
       </main>
-
-
     </div>
   );
 };

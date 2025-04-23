e70a4153c3662762774d335aef5016334da692f5
martin-peralta <martperalta@alumnos.uai.cl>
2025-04-23 13:07:55 -0400

    PPTR-67 Ruteo de cartas en backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './TRdb.js';
 import authRoutes from './routes/authRoutes.js';
+import cardRoutes from '.cardRoutes.js'; 
 
 dotenv.config();
 connectDB(); 
@@ -11,14 +12,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Rutas (user y cartas)
 app.use('/api/auth', authRoutes);
-
+app.use('/api/cards', cardRoutes); 
 
 
 app.get('/', (req, res) => {
     res.send('API funcionando correctamente');
 });
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
